fc06d80aa1c684e4a35c7dbec6bd0f57c53b040f
Alexandra LEE <palexalee@gmail.com>
2018-04-05 10:59:08 +0200

    "add"

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -10,29 +10,35 @@
 
 // Your code :
 const multiply = (a, b) => {
-  let count = 0
-  let result = b
+    let result = 0;
+    let i = 0;
+    if (a > 0) {
+      while (a > i) {
+        result = result + b;
+        i++
+      }
+      return result;
+    } else {
+      while (a < 0) {
+        result = result + b;
+        i--
+      }
+      return -result;
+    }
 
-  while (count < a) {
-    count = count - 1
-    result = result + b
-  }
+    //* Begin of tests
+    const assert = require('assert')
 
-  return result
-}
-//* Begin of tests
-const assert = require('assert')
-
-assert.strictEqual(typeof multiply, 'function')
-assert.strictEqual(multiply.length, 2)
-assert.strictEqual(multiply.toString().includes('Math.imul'), false)
-assert.strictEqual(multiply.toString().includes('*'), false)
-assert.strictEqual(multiply.toString().includes('/'), false)
-assert.strictEqual(multiply(34, 78), 2652)
-assert.strictEqual(multiply(123, 0), 0)
-assert.strictEqual(multiply(0, -230), 0)
-assert.strictEqual(multiply(0, 0), 0)
-assert.strictEqual(multiply(123, -22), -2706)
-assert.strictEqual(multiply(-22, 123), -2706)
-assert.strictEqual(multiply(-22, -123), 2706)
-// End of tests */
+    assert.strictEqual(typeof multiply, 'function')
+    assert.strictEqual(multiply.length, 2)
+    assert.strictEqual(multiply.toString().includes('Math.imul'), false)
+    assert.strictEqual(multiply.toString().includes('*'), false)
+    assert.strictEqual(multiply.toString().includes('/'), false)
+    assert.strictEqual(multiply(34, 78), 2652)
+    assert.strictEqual(multiply(123, 0), 0)
+    assert.strictEqual(multiply(0, -230), 0)
+    assert.strictEqual(multiply(0, 0), 0)
+    assert.strictEqual(multiply(123, -22), -2706)
+    assert.strictEqual(multiply(-22, 123), -2706)
+    assert.strictEqual(multiply(-22, -123), 2706)
+    // End of tests */
